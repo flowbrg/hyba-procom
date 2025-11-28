0c296c2c8b24f21218ecb38f875c5d47e2e74ea7
Florent Bergé <florent.berge@gmail.com>
2025-11-28 11:33:20 +0100

    Add: introduce new powertrain model and update variables_procom for clarity

--- src/bateau/variables_procom.m
+++ src/bateau/variables_procom.m
@@ -1,13 +1,13 @@
-% Variables modèle coque 
-m = 250000; % Masse navire
-Ma = 123434; % Masse ajoutée
+%Variables modèle coque 
+m = 250000; %masse navire
+ma = 123434; %masse ajoutée
 
 
-% Variables modèle hélice
-rho = 1025; % Masse volumique de l'eau
+%Variables modèle hélice
+rho = 1025; %Masse volumique de l'eau
 
-prop = PROP.Ka3_65_19A_D_2_07; % Choix de la référence de l'hélice
-hull = HULL.Naoned_hull; % Initialisation de la coque
+prop = PROP.Ka3_65_19A_D_2_07;
+hull = HULL.Naoned_hull;
 
 x = prop.x;
 y = prop.y;
@@ -19,37 +19,46 @@ D = prop.D;
 
 
 
-% Modèle moteur thermique avec temps de réponse désiré de 4s
-nmt_nom = 750; % Régime nominal du moteur
+%Modèle moteur thermique avec temps de réponse désiré de 4s
+nmt_nom = 750; %Vitesse rotation nominale du moteur
 
-Rt = 1.8; % Résistance moteur thermique
-Jt = 2.22; % Charge moteur thermique
+R = 1/2.953; %rendement moteur diesel seul
 
-% Modèle moteur thermique avec temps de réponse désiré de 0.4s
-Re = 1.5; % Résistance moteur électrique
-Je = 0.6; % Charge moteur electrique
+tau_mt = 4;
 
-% Modèle de la transmission
+%Rt = 1.8; %résistance moteur thermique %pas utilisé avec nouveau modèle
+Jmt = 2.22; %resistance moteur thermique
+
+%Modèle moteur thermique avec temps de réponse désiré de 0.4s
+% Re = 1.5; %résistance moteur électrique %pas utilisé avec nouveau modèle
+% Je = 0.6; %resistance moteur electrique %pas utilisé dans la v1
+
+
+
+%Modele Transmission
 Rmt = 4.6680;
 Rme = 6.6080;
+eta_mt = 0.4; %rendement maximal moteur thermique
 
-eta_m = 0.4; % Rendement maximal moteur thermique
-R = 1/2.953;
-Pyf = 60;
-Pyv = 180;
+Jprop = Jmt/R^2;
+
+Pe = 5; %Puissance alternateurs
+Py = 60; % valeur en filage car ligne droite
 eta_p = 0.5;
 eta_r = 0.96;
-Pe = 5;
 
-% Coefficients PID du speed pilot 
+
+%Coefficients PID du speed pilot 
 kp = 3500;
 ki = 700;
 kd = 0;
 
 
+
+%Modèle chalut
+Tc = 1; % poussée du chalut
+
+
+%Servent à quelque chose ?
+
 eta_b = 1;
-
-% Modèle chalut
-Tc = 1; % Poussée du chalut
-
-
