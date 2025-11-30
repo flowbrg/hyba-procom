2a18b608c2361d4593c2b24d019aabcab913fae3
Florent Bergé <florent.berge@gmail.com>
2025-11-30 13:23:31 +0100

    Refactor code structure for improved readability and maintainability

--- src/bateau/variables_procom.m
+++ src/bateau/variables_procom.m
@@ -1,10 +1,12 @@
-%Variables modèle coque 
-m = 250000; %masse navire
-ma = 123434; %masse ajoutée
+INIT_json2matlab
+
+% Variables modèle coque 
+m = 250000; % Masse navire
+ma = 123434; % Masse ajoutée
 
 
-%Variables modèle hélice
-rho = 1025; %Masse volumique de l'eau
+% Variables modèle hélice
+rho = 1025; % Masse volumique de l'eau
 
 prop = PROP.Ka3_65_19A_D_2_07;
 hull = HULL.Naoned_hull;
@@ -19,26 +21,26 @@ D = prop.D;
 
 
 
-%Modèle moteur thermique avec temps de réponse désiré de 4s
-nmt_nom = 750; %Vitesse rotation nominale du moteur
+% Modèle moteur thermique avec temps de réponse désiré de 4s
+nmt_nom = 750; % Vitesse rotation nominale du moteur
 
-R = 1/2.953; %rendement moteur diesel seul
+R = 1/2.953; % Rapport de réduction du moteur diesel seul
 
-tau_mt = 4;
+tau_mt = 0.4;
 
-%Rt = 1.8; %résistance moteur thermique %pas utilisé avec nouveau modèle
-Jmt = 2.22; %resistance moteur thermique
+%Rt = 1.8; % Résistance moteur thermique %pas utilisé avec nouveau modèle
+Jmt = 2.22; % Résistance moteur thermique (kg.m2)
 
-%Modèle moteur thermique avec temps de réponse désiré de 0.4s
-% Re = 1.5; %résistance moteur électrique %pas utilisé avec nouveau modèle
-% Je = 0.6; %resistance moteur electrique %pas utilisé dans la v1
+% Modèle moteur thermique avec temps de réponse désiré de 0.4s
+% Re = 1.5; % Résistance moteur électrique %pas utilisé avec nouveau modèle
+% Je = 0.6; % Résistance moteur electrique %pas utilisé dans la v1
 
 
 
-%Modele Transmission
+% Modele Transmission
 Rmt = 4.6680;
 Rme = 6.6080;
-eta_mt = 0.4; %rendement maximal moteur thermique
+eta_mt = 0.4; % Rendement maximal moteur thermique
 
 Jprop = Jmt/R^2;
 
@@ -51,12 +53,10 @@ eta_r = 0.96;
 %Coefficients PID du speed pilot 
 kp = 3500;
 ki = 700;
-kd = 0;
-
 
 
 %Modèle chalut
-Tc = 1; % poussée du chalut
+Tc = 5.8e3; % poussée du chalut
 
 
 %Servent à quelque chose ?
